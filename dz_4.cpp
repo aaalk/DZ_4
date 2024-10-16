c429171eeb84b6b77b0c7a27ce501b9e6bd9d083
aaalk <115035998+aaalk@users.noreply.github.com>
2024-10-16 23:11:30 +0300

    Add files via upload

---
+++ dz_4.cpp
@@ -0,0 +1,161 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+
+using namespace std;
+double pi = 3.14159265358979323846;
+
+void task_1() {
+    ofstream fout;
+    fout.open("chisla.txt");
+    double l;
+    double summ = 0;
+    cout << "Введите 10 чисел" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> l;
+        fout << l << endl;
+    }
+    fout.close();
+    double str;
+    ifstream fin;
+    fin.open("chisla.txt");
+    while (fin >> str)
+    {
+        summ += str;
+    }
+    fin.close();
+    cout << "Сумма = " << summ << endl;
+}
+
+
+int task_2(double x) {
+    int a = 0;
+    if (x < 0) {
+        a = -1;
+    }
+    else if (x == 0) {
+        a = 0;
+    }
+    else {
+        a = 1;
+    }
+    return a;
+}
+
+
+
+double pr(double a, double b) { 
+    return a * b;
+}
+
+double tr(double a, double h) { 
+    return 0.5 * a * h;
+}
+
+
+double kr(double r) { 
+    return pi * pow(r, 2);
+}
+
+
+void task_3() {
+    double a = 0, b = 0, h = 0, r = 0, c = 0;
+    cout << "Введите стороны прямоугольника: " << endl;
+    cin >> a >> b;
+    cout << "Введите высоту и сторону треугольника: " << endl;
+    cin >> h >> c;
+    cout << "Введите радиус круга: " << endl;
+    cin >> r;
+    if (a > 0 && b > 0 && h > 0 && c > 0 && r > 0) {
+        cout << "Площадь прямоугольника = " << pr(a, b) << endl;
+        cout << "Площадь треугольника = " << tr(h, c) << endl;
+        cout << "Площадь круга = " << kr(r) << endl;
+    }
+    else {
+        cout << "Ошибка";
+    }
+}
+
+
+void task_4() {
+    for (int c = 1; c <= 4; c++) {
+        for (int a = 1; a <= 12; a++) {
+            cout << "*";
+        }
+        for (int b = 1; b <= 30; b++) {
+            cout << "_";
+        }
+        cout << endl;
+    }
+    for (int d = 1; d <= 9; d++) {
+        for (int e = 1; e <= 42; e++) {
+            cout << "_";
+        }
+        cout << endl;
+    }
+}
+
+
+
+int digit(char x) {
+    switch (x) {
+    case 'I': return 1;
+    case 'V': return 5;
+    case 'X': return 10;
+    case 'L': return 50;
+    case 'C': return 100;
+    case 'D': return 500;
+    case 'M': return 1000;
+    }
+    return -1;
+}
+void task_6() {
+    string num = "";
+    int len = 0, summ = 0;
+    cout << "Введите римское число: " << endl;
+    cin >> num;
+    len = size(num);
+    for (int i = 0; i < len; i++) {
+        if (digit(num[i]) >= digit(num[i + 1])) {
+            summ += digit(num[i]);
+            }
+        else if (digit(num[i]) < digit(num[i + 1])) {
+            summ -= digit(num[i]);
+
+        }
+    }
+    cout << summ;
+}
+void task_5() {
+    int  V = 25;
+    int  S = 80;
+    char arr[25][80];
+    for (int y = 0; y < V; ++y) {
+        for (int x = 0; x < S; ++x) {
+            int y = (sin((x * 2 * pi) / S) + 1) / 2 * V;
+            if ((y >= 0) && (y < V)) {
+                arr[y][x] = '*';
+            }
+        }
+        for (y = 0; y < V; ++y) {
+            for (int x = 0; x < S; ++x)
+                cout << arr[y][x];
+            cout << endl;
+        }
+    }
+}
+
+int main()
+{
+    setlocale(0, "");
+    /*cout << "Введите число: " << endl;
+    double x = 0;
+    cin >> x;
+    cout << task_2(x);*/
+    task_6();
+
+}
---
+++ dz_4_2.cpp
@@ -0,0 +1,341 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void task_7();
+void task_8();
+void convert_to_int();
+void convert_to_char();
+bool chek();
+int ten();
+void task_9();
+void task_6();
+int digit();
+string convert();
+
+
+bool chek(string s)
+{
+
+	string rim[13] = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+	int k= 0, i = 0;
+
+	for (k, i; k < s.length() && i < 13; i++)
+	{
+		int rl = rim[i].length();
+		if (s.substr(k, rl) == rim[i])
+		{
+			if (rl == 1 && i != 0)
+			{
+				int cnt = 1;
+				while (k++ < s.length() && s.substr(k, rl) == rim[i]) cnt++;
+				if (cnt > 3)
+				{
+					k--;
+					break;
+				}
+			}
+			else k += rl;
+		}
+	}
+	if (k < s.length()) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+int digit(char x) {
+	switch (x) {
+	case 'I': return 1;
+	case 'V': return 5;
+	case 'X': return 10;
+	case 'L': return 50;
+	case 'C': return 100;
+	case 'D': return 500;
+	case 'M': return 1000;
+	}
+	return -1;
+}
+
+void task_6() {
+	int len = 0, summ = 0;
+	string s;
+
+	cout << "Введите римское число: ";
+	cin >> s;
+	len = size(s);
+
+	if (chek(s)) {
+		for (int k = 0; k < len; k++) {
+			if (digit(s[k]) >= digit(s[k + 1])) {
+				summ += digit(s[k]);
+			}
+			else if (digit(s[k]) < digit(s[k + 1])) {
+				summ -= digit(s[k]);
+
+			}
+		}
+		cout << summ;
+	}
+	else cout << "Такого римского числа не существует";
+}
+
+void task_7() {
+	int m1 = 37, b1 = 3, c1 = 64, m2 = 25173, b2 = 13849, c2 = 65537, i = 0, i1 = 0, i2 = 0; 
+	cout << "Введите i = ";
+	cin >> i;
+	int s = 0;
+	cout << "Вариант 1: " << endl;
+	for (int k = 1; k <= i; k++) {
+		s = (m1 * s + b1) % c1;
+		cout << s << endl;
+	}
+	cout << "Вариант 2: " << endl;
+	for (int k = 1; k <= i; k++) {
+		s = (m1 * s + b1) % c1;
+		cout << s << endl;
+	}
+}
+
+void task_8() {
+	double A[3][4] = { 
+	   {5, 2, 0, 10},
+	   {3, 5, 2, 5},
+	   {20, 0, 0, 0} };
+
+	double B[4][2] = { 
+		{1.20, 0.50},
+		{2.80, 0.40},
+		{5.00, 1.00},
+		{2.00, 1.50} };
+
+	double C[3][2];
+	
+	for (int i = 0; i <= 2; i++)
+	{
+		for (int j = 0; j <= 1; j++)
+		{
+			C[i][j] = A[i][0] * B[0][j] + A[i][1] * B[1][j] + A[i][2] * B[2][j] + A[i][3] * B[3][j];
+		}
+	}
+	cout << "Матрица С (A*B): " << endl;
+	for (int i = 0; i <= 2; i++)
+	{
+		for (int j = 0; j <= 1; j++)
+		{
+			cout << C[i][j] << " ";
+		}
+		cout << endl;
+	}
+	double maxpr = max(C[0][0], max(C[1][0], C[2][0]));
+	double minpr = min(C[0][0], min( C[1][0], C[2][0]));
+	for (int i = 0; i < 3; i++) {
+		if (maxpr == C[i][0]) {
+			cout << "Максимаьная выручка с продажи у продавца " << i+1 << endl;
+		}
+		if (minpr == C[i][0]) {
+			cout << "Минимальная выручка с продажи у продавца " << i+1 << endl;
+		}
+	}
+	double maxcom = max(C[0][1], max(C[1][1], C[2][1]));
+	double mincom = min(C[0][1], min(C[1][1], C[2][1]));
+	for (int i = 0; i < 3; i++) {
+		if (maxcom == C[i][1]) {
+			cout << "Максимальные комиссионные у продавца " << i+1 << endl;
+		}
+		if (mincom == C[i][1]) {
+			cout << "Минимальные комиссионные у продавца " << i+1 << endl;
+		}
+	}
+	cout << "Вcего выручки " << C[0][0] + C[1][0] + C[1][0] << endl;
+	cout << "Всего комиссионных получили " << C[0][1] + C[1][1] + C[2][1] << endl;
+	cout << "Всего денег прошло " << C[0][0] + C[0][1] + C[0][2] - C[0][1] - C[1][1] - C[2][1] << endl;
+
+}
+
+int convert_to_int(char x) {
+	switch (x) {
+		case '1': 
+			return 1;
+			break;
+		case '2': 
+			return 2;
+			break;
+
+		case '3': 
+			return 3;
+			break;
+
+		case '4': 
+			return 4;
+			break;
+
+		case '5': 
+			return 5;
+			break;
+
+		case '6':
+			return 6;
+			break;
+
+		case '7': 
+			return 7;
+			break;
+
+		case '8': 
+			return 8;
+			break;
+
+		case '9': 
+			return 9;
+			break;
+
+		case 'A': 
+			return 10;
+			break;
+
+		case 'B': 
+			return 11;
+			break;
+
+		case 'C': 
+			return 12;
+			break;
+
+		case 'D': 
+			return 13;
+			break;
+
+		case 'E': 
+			return 14;
+			break;
+
+		case 'F': 
+			return 15;
+			break;
+
+	}
+	return -1;
+}
+
+//int convert_to_char(int x) {
+//	switch (x) {
+//	case 1:
+//		return '1';
+//		break;
+//	case 2:
+//		return '2';
+//		break;
+//
+//	case 3:
+//		return '3';
+//		break;
+//
+//	case 4:
+//		return '4';
+//		break;
+//
+//	case 5:
+//		return '5';
+//		break;
+//
+//	case 6:
+//		return '6';
+//		break;
+//
+//	case 7:
+//		return '7';
+//		break;
+//
+//	case 8:
+//		return '8';
+//		break;
+//
+//	case 9:
+//		return '9';
+//		break;
+//
+//	case 10:
+//		return 'A';
+//		break;
+//
+//	case 11:
+//		return 'B';
+//		break;
+//
+//	case 12:
+//		return 'C';
+//		break;
+//
+//	case 13:
+//		return 'D';
+//		break;
+//
+//	case 14:
+//		return 'E';
+//		break;
+//
+//	case 15:
+//		return 'F';
+//		break;
+//
+//		return -1;
+//	}
+//}
+
+//double ten(string x, int num) {
+//	double new_x = 0;
+//	for (int i = 0; i < x.size(); i++) {
+//		int s = convert_to_int(x[i]);
+//		new_x += s * pow(num, x.size() - i - 1);
+//	}
+//	return new_x;
+//
+//}
+
+//string new_num(int x, int num) {
+//	int a = x % num;
+//	string new_x = convert_to_char(a);
+//	return new_num(x / num) + new_x;
+//
+//	new_x = "";
+//	return new_x;
+//}
+
+void task_9() {
+	string x;
+	int star = 0, nov = 0;
+	cout << "Введите число: " << endl;
+	cin >> x;
+	cout << "Введите старое и новое основание: " << endl;
+	cin >> star >> nov;
+	if (star < 2 || nov < 2) {
+		cout << "Ошибка" << endl;
+	}
+	else {
+		int new_x = 0;
+		for (int i = 0; i < x.size(); i++) {
+			int s = convert_to_int(x[i]);
+			new_x += s * pow(star, x.size() - i - 1);
+		}
+		string digits = "0123456789ABCDEF";
+		if (new_x == 0) {
+			cout << "0";
+		}
+		string res = "";
+		while (new_x > 0) {
+			res = digits[new_x % nov] + res;
+			new_x /= nov;
+		}
+		cout << res;
+	}
+}
+
+
+int main()
+{
+	setlocale(0, "");
+	task_6();
+}
